ffbd870f5d0c48be44a7e1800d2b4d416c685819
Samuel Olorunfemi <forfemi@gmail.com>
2023-12-14 14:07:22 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view/>
+  </main>
+  
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira Sans', sans-serif;
+    
+    &::selection {
+      background: transparentize(#42B883, 0.5);
     }
   }
-}
+
+  body {
+    background-color: #35495E;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  header 
 </style>
